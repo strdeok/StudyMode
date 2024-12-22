dd4e7466a8ceea86d54ef61358f83045f0c542ac
strdeok <ejraks1548@gmail.com>
2024-12-23 06:45:02 +0900

    수정

--- src/photos.jsx
+++ src/photos.jsx
@@ -1,29 +1,24 @@
 import { createClient } from "pexels";
 import { useEffect, useState } from "react";
 
+export default function Photo({ photo, setPhoto }) {
+  const client = createClient(import.meta.env.VITE_API_KEY);
 
-export default function Photo({photo, setPhoto}) {
-  
-
-  const client = createClient(
-    import.meta.env.VITE_API_KEY
-  );
-
-  function getPhoto() {
-    client.collections
+  const getPhoto = async () => {
+    await client.collections
       .media({ id: "sn4q2si", type: "photos" })
       .then((result) => {
-        const photos = photo
-        console.log(result)
-        result.media.forEach(element => {
-          photos.push(element.src.landscape)
+        const photos = photo;
+        console.log(result);
+        result.media.forEach((element) => {
+          photos.push(element.src.landscape);
         });
-        setPhoto([...photos])
+        setPhoto([...photos]);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
   useEffect(() => {
     getPhoto();
